036bcc1dacc5010fa7a291936d3ff5c3a4161e6d
gabriel-q-wang <gwgwang2009@gmail.com>
2025-08-31 15:34:24 -0700

    Always set screen to turn on.

--- buttons.py
+++ buttons.py
@@ -18,18 +18,5 @@ def turnOffScreen():
     GPIO.output(18, GPIO.LOW)
 
 
-turnOffScreen()
-screen_on = False
-
-while (True):
-    # If you are having and issue with the button doing the opposite of what you want
-    # IE Turns on when it should be off, change this line to:
-    # input = not GPIO.input(26)
-    input = GPIO.input(26)
-    if input != screen_on:
-        screen_on = input
-        if screen_on:
-            turnOnScreen()
-        else:
-            turnOffScreen()
-    time.sleep(0.3)
+turnOnScreen()
+screen_on = True
