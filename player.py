6be4f01c4a4e50dc695e9b0ce86a0cf2596b83a8
gabriel-q-wang <gwgwang2009@gmail.com>
2025-09-02 22:40:23 -0700

    Add input fixes.

--- player.py
+++ player.py
@@ -2,23 +2,22 @@ import os
 import random
 import time
 import RPi.GPIO as GPIO
-from subprocess import PIPE, Popen, STDOUT
+import subprocess
 
-directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'videos/encoded')
+directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'videos/encoded')                                                                                                                                                                                   videos = []
 
-videos = []
-
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.OUT)
+GPIO.output(23, GPIO.HIGH) # Set high
+time.sleep(1)
+GPIO.output(23, GPIO.LOW)  # Set low
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def skipCurrentVideo(counter):
+def skipCurrentVideo(previous_state):
     # Button assigned to GPIO 23
+    time.sleep(0.5) # Debounce delay
     input = GPIO.input(23)
-    # Button needs to be pressed twice to skip the video.
-    if input == GPIO.LOW: # For active-low button
-        counter += 1
-    if input == GPIO.HIGH:
-        counter += 1
-    if counter >= 2:
+    if previous_state != input:
         return True
     return False
 
@@ -38,14 +37,30 @@ def playVideos():
         time.sleep(5)
         return
     random.shuffle(videos)
-    counter = 0
+    previous_state = GPIO.input(23)
+    playProcess = None
     for video in videos:
-        playProcess = Popen(['omxplayer', '--no-osd', '--aspect-mode', 'fill', video])
-        while playProcess.poll() is None:
-        # Check your condition here
-            if skipCurrentVideo(counter):
-                counter = 0
-                playProcess.terminate()  # Or process.kill() for a more forceful termination
+        if playProcess is None:  
+            playProcess = subprocess.Popen(['omxplayer', '--no-osd', '--aspect-mode', 'fill', video], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        while playProcess is not None:
+            if skipCurrentVideo(previous_state):
+                try:
+                    playProcess.stdin.write(b'q')
+                    playProcess.stdin.flush()
+                except BrokenPipeError:
+                    # This can happen if the process has already exited
+                    pass
+                previous_state = GPIO.input(23)
+                playProcess.wait()
+                time.sleep(1)
+                # If the process is still running, force-terminate it
+                if playProcess.poll() is None:
+                    playProcess.terminate()
+                    time.sleep(1)  # Give it a moment to stop
+                    if playProcess.poll() is None:
+                        playProcess.kill()
+                time.sleep(0.5)
+                playProcess = None
                 break
             time.sleep(1) # Wait for a second before re-checking
 
