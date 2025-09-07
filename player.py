6508e569b1d8e7342bdea0c65ed2e84b7aec48a4
gabriel-q-wang <gwgwang2009@gmail.com>
2025-09-06 19:32:35 -0700

    Reset curr_player to None to fully kill player.

--- player.py
+++ player.py
@@ -76,10 +76,11 @@ def playVideos():
         next_dbus = DBUS_NAME_2 if curr_dbus else DBUS_NAME_1
         next_player = setup_player(next_video, next_dbus)
         curr_player.play()
-        while curr_player.playback_status == "Playing":
+        while curr_player is not None:
             if skipCurrentVideo(previous_state):
                 next_player.play()
                 curr_player.quit()
+                curr_player = None
                 previous_state = GPIO.input(23)
                 curr_dbus = not curr_dbus
                 curr_player, next_player = next_player, None
@@ -89,6 +90,7 @@ def playVideos():
             if remaining_time < 3:
                 next_player.play()
                 curr_player.quit()
+                curr_player = None
                 # Swap the players for the next iteration
                 curr_player, next_player = next_player, None
                 curr_dbus = not curr_dbus
